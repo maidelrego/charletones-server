00a3f561a3788e795813d752c4cf3d3b3995658b
Maydel Rego <maidelrego@yahoo.com>
2023-03-02 18:10:23 -0600

    Revert "checking for auto prod deploy"
    This reverts commit a177cb0cf5eb33f1dad2c89e62040ba42942f8fb.

--- src/auth/decorators/role-protected.decorator.ts
+++ src/auth/decorators/role-protected.decorator.ts
@@ -4,5 +4,6 @@ import { ValidRoles } from '../interfaces/valid-roles.interfece';
 export const META_ROLES = 'roles'
 
 export const RoleProtected = (...args: ValidRoles[]) => {
+
     return SetMetadata(META_ROLES, args);
 };
